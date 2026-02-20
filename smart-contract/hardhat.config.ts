0595615c6a4363e68a9fa0d9078c39efeba8c4be
Yo-930 <yogeshsuryagm@gmail.com>
2026-02-20 21:25:37 +0530

    contracts

---
+++ smart-contract/hardhat.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "hardhat/config";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
+export default defineConfig({
+  solidity: "0.8.20",
+});
